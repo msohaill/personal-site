3cea04fbc1b2442392c853683601d227de418a7d
muhammadsohail <mhsohail56@gmail.com>
2022-05-29 18:42:14 -0400

    formatting nav

--- src/components/Navabar/index.tsx
+++ src/components/Navabar/index.tsx
@@ -4,37 +4,40 @@ import { useContext, useState } from 'react';
 import { ThemeContext } from '../../contexts/Theme';
 
 const Navbar = () => {
-    const { theme, toggleTheme } = useContext(ThemeContext);
-    const [navVisible, setNavVisible] = useState(false);
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  const [navVisible, setNavVisible] = useState(false);
 
-    const toggleNavList = () => setNavVisible((prevState) => !prevState);
+  const toggleNavList = () => setNavVisible((prevState) => !prevState);
 
-    window.onresize = () => window.innerWidth > 800 && navVisible && toggleNavList();
+  window.onresize = () => window.innerWidth > 800 && navVisible && toggleNavList();
 
-    return (
-        <nav className='center nav-bar'>
-            <ul className='nav-list' style={{ display: navVisible ? 'flex' : undefined }}>
-                <li>
-                    <a className='nav-link'>experience</a>
-                </li>
-                <li>
-                    <a className='nav-link'>projects</a>
-                </li>
-                <li>
-                    <a className='nav-link'>skills</a>
-                </li>
-                <li>
-                    <a className='nav-link'>contact</a>
-                </li>
-            </ul>
-            <button className='nav-theme' type='button' onClick={toggleTheme}>
-                {theme === 'dark' ? <WbTwilightRounded /> : <NightsStayRounded />}
-            </button>
-            <button className='nav-menu' type='button' onClick={toggleNavList}>
-                <Menu />
-            </button>
-        </nav>
-    );
+  return (
+    <nav className='center nav-bar'>
+      <ul className='nav-list' style={{ display: navVisible ? 'flex' : undefined }}>
+        <a className='nav-link'>
+          <li>experience</li>
+        </a>
+        <a className='nav-link'>
+          <li>projects</li>
+        </a>
+        <a className='nav-link'>
+          <li>skills</li>
+        </a>
+        <a className='nav-link'>
+          <li>contact</li>
+        </a>
+        <a className='nav-link'>
+          <li>photos</li>
+        </a>
+      </ul>
+      <button className='nav-theme' type='button' onClick={toggleTheme}>
+        {theme === 'dark' ? <WbTwilightRounded /> : <NightsStayRounded />}
+      </button>
+      <button className='nav-menu' type='button' onClick={toggleNavList}>
+        <Menu />
+      </button>
+    </nav>
+  );
 };
 
 export default Navbar;
