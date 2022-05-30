412e568f228537882daba0a5d0cd41a5f6af248e
muhammadsohail <mhsohail56@gmail.com>
2022-05-30 11:05:45 -0400

    resetting build issues; adding photos

--- src/components/Photos/index.tsx
+++ src/components/Photos/index.tsx
@@ -1,14 +1,24 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { ThemeContext } from '../../contexts/Theme';
 import Header from '..//Header';
 import { Masonry } from '@mui/lab';
 
+const shuffleArray = (arr: any[]) => {
+  let i = arr.length - 1;
+  for (; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+  return arr;
+};
+
 const r = require.context('../../assets/images/gallery', false, /\.(png|jpe?g|svg)$/i);
-const images = r.keys().map(r) as string[];
+const images = shuffleArray(r.keys().map(r)) as string[];
 
 const Photos = () => {
   const { theme } = useContext(ThemeContext);
-  const [loaded, setLoaded] = useState(false);
 
   window.onscroll = () => {
     const header = document.getElementsByTagName('header')[0];
@@ -20,15 +30,14 @@ const Photos = () => {
     <div className={`page ${theme} center`}>
       <Header showMain={false} />
       <main className='content'>
-        <Masonry columns={{ xs: 1, sm: 2, lg: 3 }} spacing={1} style={{ display: loaded ? undefined : 'none' }}>
-          {images.map((item, index) => (
+        <Masonry id='gallery' columns={{ xs: 1, sm: 2, lg: 3 }} spacing={1}>
+          {images.map((item, i) => (
             <img
-              src={`${item}?w=248&fit=crop&auto=format`}
-              srcSet={`${item}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              src={item}
+              height='default'
               alt={item.substring(item.lastIndexOf('/') + 1).replace(/(\..*)(\.(png|jpe?g|svg))$/, '$2')}
-              loading='eager'
-              onLoad={() => index === 4 && setLoaded(true)}
               key={item.substring(item.lastIndexOf('/') + 1).replace(/(\..*)(\.(png|jpe?g|svg))$/, '$2')}
+              loading={i < 5 ? 'eager' : 'lazy'}
             />
           ))}
         </Masonry>
