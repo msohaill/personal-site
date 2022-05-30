71abeac6fe7f2c4d7359906b4683116e380c0f78
muhammadsohail <mhsohail56@gmail.com>
2022-05-30 03:47:19 -0400

    trying out loading

--- src/components/Photos/index.tsx
+++ src/components/Photos/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { ThemeContext } from '../../contexts/Theme';
 import Header from '..//Header';
 import { Masonry } from '@mui/lab';
@@ -8,6 +8,7 @@ const images = r.keys().map(r) as string[];
 
 const Photos = () => {
   const { theme } = useContext(ThemeContext);
+  const [loaded, setLoaded] = useState(false);
 
   window.onscroll = () => {
     const header = document.getElementsByTagName('header')[0];
@@ -19,13 +20,15 @@ const Photos = () => {
     <div className={`page ${theme} center`}>
       <Header showMain={false} />
       <main className='content'>
-        <Masonry columns={{ xs: 1, sm: 2, lg: 3 }} spacing={1}>
-          {images.map((item) => (
+        <Masonry columns={{ xs: 1, sm: 2, lg: 3 }} spacing={1} style={{ display: loaded ? undefined : 'none' }}>
+          {images.map((item, index) => (
             <img
               src={`${item}?w=248&fit=crop&auto=format`}
               srcSet={`${item}?w=248&fit=crop&auto=format&dpr=2 2x`}
               alt={item.substring(item.lastIndexOf('/') + 1).replace(/(\..*)(\.(png|jpe?g|svg))$/, '$2')}
               loading='eager'
+              onLoad={() => index === 4 && setLoaded(true)}
+              key={item.substring(item.lastIndexOf('/') + 1).replace(/(\..*)(\.(png|jpe?g|svg))$/, '$2')}
             />
           ))}
         </Masonry>
