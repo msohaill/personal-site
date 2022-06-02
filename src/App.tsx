2ade5ca59ba0d9bdc13f8a9ac7f11f0e92a1c1b1
muhammadsohail <mhsohail56@gmail.com>
2022-06-02 01:53:02 -0400

    adding song feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.scss';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { ThemeContext } from './contexts/Theme';
 import Header from './components/Header';
+import { Buffer } from 'buffer';
 
 const App = () => {
   const { theme } = useContext(ThemeContext);
+  const [track, setTrack] = useState<any>({});
 
   window.onscroll = () => {
     const header = document.getElementsByTagName('header')[0];
@@ -13,17 +15,66 @@ const App = () => {
     else header.classList.remove('active');
   };
 
+  const getRandomSong = async () => {
+    const tokenResponse = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${Buffer.from(
+          process.env.REACT_APP_CLIENT_ID + ':' + process.env.REACT_APP_CLIENT_SECRET
+        ).toString('base64')}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        grant_type: 'refresh_token',
+        refresh_token: process.env.REACT_APP_refresh_token as string,
+      }),
+    });
+    const token = (await tokenResponse.json())['access_token'];
+
+    const savedTrackResponse = await fetch(
+      'https://api.spotify.com/v1/me/tracks?' +
+        new URLSearchParams({
+          limit: '1',
+          offset: Math.floor(Math.random() * 452).toString(),
+        }),
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    const returnedTrack = await savedTrackResponse.json();
+    return returnedTrack.items[0].track;
+  };
+
+  useEffect(() => {
+    getRandomSong().then((retTrack) => setTrack(retTrack));
+  }, []);
+
   return (
     <div className={`page ${theme} center`}>
       <Header showMain={true} />
       <main className='content center'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Song of the day is {track.name}!</p>
         <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
+        <iframe
+          style={{ borderRadius: 12, minWidth: 300, marginTop: 50 }}
+          src={
+            track.external_urls
+              ? `${track.external_urls.spotify.replace('.com/track', '.com/embed/track')}?utm_source=generator`
+              : ''
+          }
+          width='30%'
+          height='80'
+          frameBorder='0'
+          allowFullScreen
+          allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
+        ></iframe>
       </main>
     </div>
   );
