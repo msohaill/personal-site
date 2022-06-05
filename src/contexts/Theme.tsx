78d6b6aa4e265ac2a94ed09c8b4d05cc46bd2284
muhammadsohail <mhsohail56@gmail.com>
2022-06-05 15:22:12 -0400

    adding theme before render and saving to local storage

--- src/contexts/Theme.tsx
+++ src/contexts/Theme.tsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useLayoutEffect, useState } from 'react';
 
 interface Theme {
   theme: string;
@@ -12,15 +12,21 @@ const defaultTheme = {
 const ThemeContext = createContext<Theme>(defaultTheme);
 
 const ThemeProvider = ({ children }: { children: JSX.Element }) => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState(localStorage.getItem('theme') ?? 'light');
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    setTheme((prevTheme) => {
+      localStorage.setItem('theme', prevTheme === 'light' ? 'dark' : 'light');
+      return prevTheme === 'light' ? 'dark' : 'light';
+    });
   };
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    if (prefersDark) setTheme('dark');
+    if (prefersDark) {
+      localStorage.setItem('theme', 'dark');
+      setTheme('dark');
+    }
   }, []);
 
   return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
