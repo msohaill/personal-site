1a9fbd1f73132fbfd08ebe5d80537ce2b60f12a5
muhammadsohail <mhsohail56@gmail.com>
2022-07-13 00:04:13 -0400

    adding skills

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -25,42 +25,42 @@ const Skills = () => {
     });
   }, []);
 
+  const skills = [
+    { name: 'Python', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg' },
+    { name: 'Git', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg' },
+    {
+      name: 'PostgreSQL',
+      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg',
+    },
+    { name: 'Java', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg' },
+    {
+      name: 'JavaScript',
+      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+    },
+    {
+      name: 'TypeScript',
+      src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
+    },
+    { name: 'CSS', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg' },
+    { name: 'HTML', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg' },
+    { name: 'Django', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg' },
+    { name: 'C', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg' },
+    { name: 'Bash', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg' },
+    { name: 'Unix/Linux', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg' },
+    { name: 'React', src: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg' },
+  ];
+
   return (
     <MainSection heading='skills' headingId='skills'>
       <div className='skill-container'>
-        <div className='skill-box-wrapper'>
-          <div className='skill-box'>Python</div>
-        </div>
-        <div className='skill-box-wrapper'>
-          <div className='skill-box'>Redux</div>
-        </div>
-        <div className='skill-box-wrapper'>
-          <div className='skill-box'>PostgreSQL</div>
-        </div>
-        <div className='skill-box-wrapper'>
-          <div className='skill-box'>Java</div>
-        </div>
-        <div className='skill-box-wrapper'>
-          <div className='skill-box'>JavaScript</div>
-        </div>
-        <div className='skill-box-wrapper'>
-          <div className='skill-box'>TypeScript</div>
-        </div>
-        <div className='skill-box-wrapper'>
-          <div className='skill-box'>CSS</div>
-        </div>
-        <div className='skill-box-wrapper'>
-          <div className='skill-box'>HTML</div>
-        </div>
-        <div className='skill-box-wrapper'>
-          <div className='skill-box'>Django</div>
-        </div>
-        <div className='skill-box-wrapper'>
-          <div className='skill-box'>C</div>
-        </div>
-        <div className='skill-box-wrapper'>
-          <div className='skill-box'>Bash</div>
-        </div>
+        {skills.map((e) => (
+          <div className='skill-box-wrapper'>
+            <div className='skill-box'>
+              <img className='skill-img' src={e.src} />
+              <h4>{e.name}</h4>
+            </div>
+          </div>
+        ))}
       </div>
     </MainSection>
   );
