5435c7089866115b0cbce51c8260aac6c07aae91
muhammadsohail <mhsohail56@gmail.com>
2023-02-19 03:53:44 -0500

    Adding error 404

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { Bug } from "lucide-svelte";
+</script>
+
+<div class="layout">
+  <div
+    class="py-20 text-xl flex justify-center items-center"
+  >
+    <Bug class="mr-2" />
+    <span class="font-medium">{$page.status}</span>
+    <div class="mx-4 h-8 border-l border-black" />
+    {$page.error?.message}
+  </div>
+</div>
