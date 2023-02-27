0e2c6a503c3a8d61c11f29462a7eeeeac69e151c
muhammadsohail <mhsohail56@gmail.com>
2023-02-26 21:01:46 -0500

    Adding music in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,36 @@
-<script>
-  import { Linkedin, Github, Mail } from 'lucide-svelte';
+<script lang="ts">
+  import { Linkedin, Github, Mail, ExternalLink } from 'lucide-svelte';
+  import { getRandomSong } from '$lib/utils';
+  import { onMount } from 'svelte';
+  import { slide } from 'svelte/transition';
+
+  const track: {
+    artists: string;
+    audio: HTMLAudioElement | null;
+    link: string;
+    title: string;
+  } = {
+    artists: '',
+    audio: null,
+    link: '',
+    title: '',
+  };
+
+  onMount(() =>
+    getRandomSong().then((trackResponse) => {
+      track.artists = trackResponse.artists.map((a: { name: string }) => a.name).join(', ');
+      track.audio = new Audio(trackResponse.preview_url);
+      track.link = trackResponse.external_urls.spotify;
+      track.title = trackResponse.name;
+    })
+  );
+
+  $: showMusic = track.audio && !track.audio.paused;
+
+  const handleClick = () => {
+    track.audio?.paused && !showMusic ? track.audio.play() : track.audio?.pause();
+    track.audio = track.audio;
+  };
 </script>
 
 <footer class="layout mt-20 flex flex-col">
@@ -28,9 +59,25 @@
       </a>
     </div>
   </div>
-  <p class="mx-auto mt-5 text-xs color text-neutral-500">
-    montréal • carpe diem • {new Date().getFullYear()}
+  <p class="mx-auto mt-5 text-xs text-neutral-500">
+    montréal • carpe diem • {new Date().getFullYear()} •
+    <span class="link cursor-pointer" on:click={handleClick} on:keydown={() => {}}>music!</span>
   </p>
+  {#if showMusic}
+    <div
+      transition:slide
+      class="flex flex-col gap-2 mx-auto mt-3 items-center border-t py-2 min-w-[60%]"
+    >
+      <em class="text-sm tracking-wide text-neutral-500 font-serif">Something I'm listening to</em>
+      <a href={track.link} target="_blank" rel="noopener noreferrer">
+        <p class="text-xs text-neutral-500">
+          <strong>{track.title}</strong>
+          {track.artists}
+          <ExternalLink size={10} class="inline align-baseline" />
+        </p>
+      </a>
+    </div>
+  {/if}
 </footer>
 
 <style lang="postcss">
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
+import { Buffer } from 'buffer';
+import {
+  PUBLIC_CLIENT_ID as CLIENT_ID,
+  PUBLIC_CLIENT_SECRET as CLIENT_SECRET,
+  PUBLIC_REFRESH_TOKEN as REFRESH_TOKEN,
+} from '$env/static/public';
+
 export const shuffleArray = <T>(arr: T[]) => {
   let i = arr.length - 1;
   for (; i > 0; i--) {
@@ -10,3 +17,36 @@ export const shuffleArray = <T>(arr: T[]) => {
 };
 
 export const basename = (name: string) => name.split(/[\\/]/).reverse()[0];
+
+export const getRandomSong = async () => {
+  const tokenResponse = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      grant_type: 'refresh_token',
+      refresh_token: REFRESH_TOKEN,
+    }),
+  });
+  const token = (await tokenResponse.json())['access_token'];
+
+  const savedTrackResponse = await fetch(
+    'https://api.spotify.com/v1/me/tracks?' +
+      new URLSearchParams({
+        market: 'CA',
+        limit: '1',
+        offset: Math.floor(Math.random() ** 2 * 561).toString(),
+      }),
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    }
+  );
+
+  const returnedTrack = await savedTrackResponse.json();
+  return returnedTrack.items[0].track;
+};
